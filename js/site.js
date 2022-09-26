b5aa9b40eb5b1f5c1dd2ec60245c79c438a635ea
Adangc <73211388+Adangc@users.noreply.github.com>
2022-09-26 14:11:14 -0700

    Responsive Navbar
    Added hamburger menu that toggles with JS and making adjustments and improvements on my class website

--- js/site.js
+++ js/site.js
@@ -1,1 +1,14 @@
 console.log("HelloWorld")
+
+//For a Responsive Nav Bar
+function ToggleMenu(){
+  var x = document.getElementById("Top-Nav");
+  if(x.className === "nav-bar") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav-bar";
+  }
+}
+
+
+document.getElementById("howds").innerHTML="HI HOWDS ";
