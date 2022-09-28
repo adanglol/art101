564caaa446e719913d03c0b447f14647c6a3a6d2
Adangc <73211388+Adangc@users.noreply.github.com>
2022-09-27 22:51:55 -0700

    Added a clock
    added clock to website using JS

--- js/site.js
+++ js/site.js
@@ -1,7 +1,7 @@
 console.log("HelloWorld")
 
 //For a Responsive Nav Bar
-
+//For the toggling class to hamburger menu to x on responsive nav
 function DropDown(el){
   if (el.className == "fa fa-bars"){
     el.className = "fa fa-times";
@@ -10,6 +10,7 @@ function DropDown(el){
   }
 }
 
+//Function to make our website responsive
 function BurgerMenu(){
   var x = document.getElementById("Top-Nav");
   if(x.className === "nav-bar") {
@@ -18,3 +19,47 @@ function BurgerMenu(){
     x.className = "nav-bar";
   }
 }
+
+//For clock on the website functionality
+function showTime(){
+  //creating a data instance using new Date() constructor
+  var date = new Date();
+  //creating a variable for each type of data hour min sec and sessions and calling them with respective function in date instance
+  var h = date.getHours();
+  var m = date.getMinutes();
+  var s = date.getSeconds();
+  var session = "AM";
+
+
+
+  //fix the clock don't want see zero in getHours
+  if (h == 0){
+    h=12;
+  }
+
+  //non military time >12 then -12 and if greater than 12am then turn to pm
+  if (h > 12){
+    h -= 12;
+    session = "PM"
+  }
+
+
+  //fixing the issue with number less than 10 in digital clock  using ternary operator ? if else statement condition true false
+  h = (h<10) ? "0" + h : h;
+  m = (m<10) ? "0" + m : m;
+  s = (s<10) ? "0" + s : s;
+
+
+
+
+
+  var time = 'You are tuning in at' + ' '+ h + ':' + m + ':' + s +' ' + session + " " + 'PST!';
+
+  document.getElementById("myClock").innerText = time;
+  document.getElementById("myClock").innerContent = time;
+
+  // set timeout
+  setTimeout(showTime,1000);
+}
+showTime();
+//setInterval(showTime,1000);
