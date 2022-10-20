e493b29ae10d9facf009138ef3ef93e83aa8dc29
Adangc <73211388+Adangc@users.noreply.github.com>
2022-10-19 18:19:17 -0700

    Lab7.js finished up almost all except anagram
    ready to turn in just pushing changes onto website

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -3,25 +3,41 @@
 // Lab 7 : Functions
 
 
-// We need to declare a function that functions to take user input
+//function is for case insenstives whether lower or upper
+function caseInsensitive(strA,strB){
+  //console.log(strA.toLowerCase().localeCompare(strB.toLowerCase())) // DEBUG
+  //used to check strA and strB which are both strings for username to check sort order in this case will return a 1 after since strA been sorted before strB from sortName function
+  return strA.toLowerCase().localeCompare(strB.toLowerCase());
+}
+
+var userName = window.prompt("Hi please tell me your name so I can fix it! ", "human");
+// function that returns our Users sorted new Name
 function sortName(){
   //Declare a variable called userName that uses window.prompt() to prompt user to type their user name and get that information
-  var userName = window.prompt("Hi please tell me your name so I can fix it! ", "human");
+
   //lets see our changes to see if this works within the console
   console.log("username =", userName);
 
-
-  //returned a variable nameSorted which is the new name that is being outputted which is scrambled up version
-  return nameSorted = userName.split("").sort().join("").toLowerCase();
-
-  //Debugging purposes
-  //console.log(nameSorted);
+  //First we need to take our input which is a string and turn to array then sort it
+  userName = userName.split("").sort();
+  //console.log(userName);
+  //After we take same input and run it thorugh caseinsenstive function as well as making sure it sorts out upper and lower case letter together
+  userName = userName.sort( caseInsensitive );
+  //console.log(userName);
+  //Concatenate our input
+  userName = userName.join("");
+  //Output
+  return userName.replace(/\s/g, ' ')
 
 }
 
-//More debugging on Output HTML
-//sortName();
-//console.log(nameSorted);
+//Created a div within our JS
+var div = document.createElement('div');
 
-//Outputing our function in our HTML
-document.writeln("Oh hey I've fixed your name: ",sortName(),"<br />");
+//Set attribute and give it a class of our linked CSS
+div.setAttribute('class','box');
+//Then we define content wihtin our div which is our function and script output
+div.innerHTML = "<h1>" + "Oh hey I've fixed your name: " + sortName() + "</h1>";
+
+//Append it to our div in my layout
+document.getElementById("script").appendChild(div);
