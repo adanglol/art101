2f18ad5d6deb7224fc67d1fdb1f27cba08d73852
Adangc <88640078+adanglol@users.noreply.github.com>
2022-11-04 00:27:07 -0700

    Lab 12
    Condensing some code and made signifigant changes got task bonus 3 and 4 contemplating to do?

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -18,16 +18,10 @@ function sortingHat(name){
     //Next we are going to declare a variable mod that takes length number of our string and divide it by and then based off that return us string
     mod = length % 4;
     //Create our if statements based on our mod value number which should either be 0,1,2,or 3 and return Gryffindor,Ravenclaw, Slytherin, and Hufflepuff
-    console.log("Our value of mod",mod);
-    if (mod == 0){
-      return "Gryffindor"
-    } else if (mod == 1) {
-      return "Ravenclaw"
-    } else if (mod == 2) {
-      return "Slytherin"
-    } else if (mod == 3) {
-      return "Hufflepuff"
-    }
+    //condensing our code
+    houseArray = ["Gryffindor","Ravenclaw","Slytherin","Hufflepuff"];
+    console.log(houseArray.length);
+    return houseArray[mod];
   }else {
     alert("Your input is not a string please try again!");
   };
@@ -37,11 +31,32 @@ button = $("#button").click(function(){
   //select our inputfield and get the value and concatenate output
   var name = $("#input").val();
   console.log(typeof name);
+  // Our varaible that runs functions and returns house name
   var house = sortingHat(name);
-  var message = "The Sorting Hat has sorted you into" + " " + house;
+  var message = "The Sorting Hat has sorted you into" + " " + house + "!";
   //Create a header when output message
   var messageHeader = $("<h1></h1>").addClass("welcome me").html(message);
   //append this message into to our div output\
   $("#output").append(messageHeader);
   $("#output").append("<br/>");
+
+  //Set up an array with each house description and index it off of mod
+  //Description is from the harry potter fandom 0 1 2 3 griff raven sly huffle
+  const houseDescribe = ["Gryffindor was one of the four Houses of Hogwarts School of Witchcraft and Wizardry and was founded by Godric Gryffindor. Gryffindor instructed the Sorting Hat to choose students possessing characteristics he most valued, such as courage, chivalry, nerve and determination, to be sorted into his house.","Ravenclaw was one of the four Houses of Hogwarts School of Witchcraft and Wizardry. Its founder was the medieval witch Rowena Ravenclaw. Members of this house were characterised by their wit, learning, and wisdom. The emblematic animal symbol was an eagle, and blue and bronze were its colours. The Head of Ravenclaw was Filius Flitwick, and the House ghost was the Grey Lady, real name Helena Ravenclaw, daughter of Rowena.","Slytherin was one of the four Houses at Hogwarts School of Witchcraft and Wizardry, founded by Salazar Slytherin. In establishing the house, Salazar instructed the Sorting Hat to pick students who had a few particular characteristics he most valued. Those characteristics included cunning, resourcefulness, leadership, and ambition.","Hufflepuff was one of the four Houses of Hogwarts School of Witchcraft and Wizardry. Its founder was the medieval witch Helga Hufflepuff. Hufflepuff was the most inclusive among the four houses, valuing hard work, dedication, patience, loyalty, and fair play rather than a particular aptitude in its members"];
+  //Append the description to our HTML lets create a paragraph tag while we are at it
+
+  //set up a if condition based off the value of our mod the text box will have the color same color as house
+  classname = ["gryffindor","ravenclaw","slytherin","hufflepuff"];
+  classStyle = classname[mod];
+  console.log(classStyle);
+  var paragraph = $("<p></p>").html(houseDescribe[mod]).addClass("content "+ classStyle );
+  $("#output").append(paragraph);
+
+  //After appending an paragraph with house description also wanted to add image of respective house
+  //first we need an array of the url of images
+  arrayImgUrl = ["img/gryffindor.gif","img/ravenclaw.gif","img/slytherin.gif","img/hufflepuff.gif"];
+  indexImg = arrayImgUrl[mod];
+  var img = $("<img/>").attr("src",indexImg).height(300).width(200);
+  $("#output").append(img);
+
 });
